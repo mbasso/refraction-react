7c9ad1204c5c063a847b348eafb11dc9c0e50a56
Matteo Basso <mbasso96@gmail.com>
2017-04-28 14:28:59 +0200

    handle promises in subscriptions

--- src/RefractionConnector.js
+++ src/RefractionConnector.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 export default class RefractionConnector extends React.Component {
-
   static propTypes = {
     actions: React.PropTypes.object,
     subscriptions: React.PropTypes.object,
@@ -9,39 +8,44 @@ export default class RefractionConnector extends React.Component {
       React.PropTypes.func,
       React.PropTypes.string,
     ]).isRequired,
-  }
+  };
 
   static contextTypes = {
     refraction: React.PropTypes.object.isRequired,
-  }
+  };
 
   static defaultProps = {
     actions: {},
     subscriptions: {},
     Component: 'div',
-  }
+  };
 
   constructor(props, context) {
     super(props, context);
-    Object.keys(props.subscriptions).forEach((key) => {
-      this[key] = (param) => {
-        this.setState(
-          Object.assign(
-            {},
-            this.state,
-            props.subscriptions[key](param, Object.assign({}, this.refs.element.props))
-          )
+    Object.keys(props.subscriptions).forEach(key => {
+      this[key] = param => {
+        const newProps = props.subscriptions[key](
+          param,
+          Object.assign({}, this.refs.element.props)
         );
+        if (newProps && typeof newProps.then === 'function') {
+          newProps.then(this.mergeState);
+        } else {
+          this.mergeState(newProps);
+        }
       };
     });
 
     this.alias = {};
-    Object.keys(props.actions).forEach((key) => {
+    Object.keys(props.actions).forEach(key => {
       const { refraction } = this.context;
       if (refraction[props.actions[key]]) {
         this.alias[key] = refraction[props.actions[key]].bind(refraction);
       } else {
-        this.alias[key] = refraction.publish.bind(refraction, props.actions[key]);
+        this.alias[key] = refraction.publish.bind(
+          refraction,
+          props.actions[key]
+        );
       }
     });
 
@@ -53,11 +57,17 @@ export default class RefractionConnector extends React.Component {
     this.context.refraction.unsubscribe(this);
   }
 
+  mergeState = obj => {
+    this.setState(Object.assign({}, this.state, obj));
+  };
+
   render() {
     const props = Object.assign({}, this.props);
     delete props.actions;
     delete props.subscriptions;
     const { Component, ...others } = props;
-    return <Component {...others} {...this.alias} {...this.state} ref="element" />;
+    return (
+      <Component {...others} {...this.alias} {...this.state} ref="element" />
+    );
   }
 }
