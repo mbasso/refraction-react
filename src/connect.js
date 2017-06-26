cd887c1e129a8cbb3bfefe4a117feb7ee17c8190
Matteo Basso <mbasso96@gmail.com>
2017-06-26 13:54:21 +0200

    fix connect for functional components

--- src/connect.js
+++ src/connect.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import RefractionConnector from './RefractionConnector';
 
-export default ({ actions, subscriptions }) => (Component) => (props) => (
-  <RefractionConnector
-    actions={actions}
-    subscriptions={subscriptions}
-    Component={Component}
-    {...props}
-  />
-);
+export default ({ actions, subscriptions }) => ConnectedComponent => {
+  let Component = ConnectedComponent;
+  if (!ConnectedComponent.prototype.render) {
+    // eslint-disable-next-line
+    class RefractionConnectorWrapper extends React.Component {
+      render() {
+        return <ConnectedComponent {...this.props} />;
+      }
+    }
+    Component = RefractionConnectorWrapper;
+  }
+  return props => (
+    <RefractionConnector
+      actions={actions}
+      subscriptions={subscriptions}
+      Component={Component}
+      {...props}
+    />
+  );
+};
